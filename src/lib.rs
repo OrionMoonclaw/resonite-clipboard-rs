7ce841a7da7d452cbdb2740831cb2eb21f37a8da
Baplar <me@baplar.fr>
2026-04-27 22:42:21 +0200

    Use the wayland feature of clipboard-rs
    Instead of manually using wl-clipboard-rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,9 @@ use anyhow::anyhow;
 
 use clipboard_rs::{Clipboard, ClipboardContext};
 use std::ffi::{CStr, c_char, c_uchar};
-use std::io::Read;
 use std::ptr::null;
 use std::slice;
 use std::sync::{Mutex, OnceLock};
-use wl_clipboard_rs::copy::{self, Options};
-use wl_clipboard_rs::paste::{self, ClipboardType, Seat, get_contents, get_mime_types};
 
 static CLIPBOARD_CTX: OnceLock<Mutex<Result<ClipboardContext>>> = OnceLock::new();
 
@@ -21,14 +18,6 @@ fn get_clipboard_ctx()
 }
 
 fn copy_auto_impl(data: &[u8]) -> Result<()> {
-    match Options::new().copy(
-        wl_clipboard_rs::copy::Source::Bytes(data.into()),
-        copy::MimeType::Autodetect,
-    ) {
-        Ok(_) => return Ok(()),
-        Err(e) => eprintln!("falling back to clipboard_rs, wl-clipboard-rs failed with error {e}"),
-    }
-
     let mime_type = infer::get(data)
         .map(|k| k.mime_type())
         .unwrap_or("application/octet-stream");
@@ -45,14 +34,6 @@ fn copy_auto_impl(data: &[u8]) -> Result<()> {
 }
 
 fn copy_text_impl(text: String) -> Result<()> {
-    match Options::new().copy(
-        wl_clipboard_rs::copy::Source::Bytes(text.clone().into_bytes().into()),
-        copy::MimeType::Autodetect,
-    ) {
-        Ok(_) => return Ok(()),
-        Err(e) => eprintln!("falling back to clipboard_rs, wl-clipboard-rs failed with error {e}"),
-    }
-
     let mut ctx_guard = get_clipboard_ctx()?;
     let ctx = ctx_guard
         .as_mut()
@@ -63,14 +44,6 @@ fn copy_text_impl(text: String) -> Result<()> {
 }
 
 fn copy_with_type_impl(data: &[u8], mime_type: &str) -> Result<()> {
-    match Options::new().copy(
-        wl_clipboard_rs::copy::Source::Bytes(data.into()),
-        copy::MimeType::Specific(mime_type.to_string()),
-    ) {
-        Ok(_) => return Ok(()),
-        Err(e) => eprintln!("falling back to clipboard_rs, wl-clipboard-rs failed with error {e}"),
-    }
-
     let mut ctx_guard = get_clipboard_ctx()?;
     let ctx = ctx_guard
         .as_mut()
@@ -82,18 +55,6 @@ fn copy_with_type_impl(data: &[u8], mime_type: &str) -> Result<()> {
 }
 
 fn available_mime_types_impl() -> Result<Vec<u8>> {
-    match get_mime_types(ClipboardType::Regular, Seat::Unspecified) {
-        Ok(t) => {
-            let concatenated = t.into_iter().fold(String::new(), |mut acc, t| {
-                acc.push_str(&t);
-                acc.push('\n');
-                acc
-            });
-            return Ok(concatenated.into_bytes());
-        }
-        Err(e) => eprintln!("falling back to clipboard_rs, wl-clipboard-rs failed with error {e}"),
-    }
-
     let mut ctx_guard = get_clipboard_ctx()?;
     let ctx = ctx_guard
         .as_mut()
@@ -110,25 +71,6 @@ fn available_mime_types_impl() -> Result<Vec<u8>> {
 }
 
 fn paste_with_type_impl(mime_type: &str) -> Result<Vec<u8>> {
-    match get_contents(
-        ClipboardType::Regular,
-        Seat::Unspecified,
-        paste::MimeType::Specific(mime_type),
-    ) {
-        Ok((mut p, _)) => {
-            let mut contents = Vec::new();
-            match p.read_to_end(&mut contents) {
-                Ok(_) => {
-                    return Ok(contents);
-                }
-                Err(e) => {
-                    eprintln!("falling back to clipboard_rs, wl-clipboard-rs failed with error {e}")
-                }
-            }
-        }
-        Err(e) => eprintln!("falling back to clipboard_rs, wl-clipboard-rs failed with error {e}"),
-    }
-
     let mut ctx_guard = get_clipboard_ctx()?;
     let ctx = ctx_guard
         .as_mut()
@@ -139,25 +81,6 @@ fn paste_with_type_impl(mime_type: &str) -> Result<Vec<u8>> {
 }
 
 fn paste_auto_impl() -> Result<Vec<u8>> {
-    match get_contents(
-        ClipboardType::Regular,
-        Seat::Unspecified,
-        paste::MimeType::Any,
-    ) {
-        Ok((mut p, _)) => {
-            let mut contents = Vec::new();
-            match p.read_to_end(&mut contents) {
-                Ok(_) => {
-                    return Ok(contents);
-                }
-                Err(e) => {
-                    eprintln!("falling back to clipboard_rs, wl-clipboard-rs failed with error {e}")
-                }
-            }
-        }
-        Err(e) => eprintln!("falling back to clipboard_rs, wl-clipboard-rs failed with error {e}"),
-    }
-
     let mut ctx_guard = get_clipboard_ctx()?;
     let ctx = ctx_guard
         .as_mut()
@@ -172,25 +95,6 @@ fn paste_auto_impl() -> Result<Vec<u8>> {
 }
 
 fn paste_text_impl() -> Result<Vec<u8>> {
-    match get_contents(
-        ClipboardType::Regular,
-        Seat::Unspecified,
-        paste::MimeType::Text,
-    ) {
-        Ok((mut p, _)) => {
-            let mut contents = Vec::new();
-            match p.read_to_end(&mut contents) {
-                Ok(_) => {
-                    return Ok(contents);
-                }
-                Err(e) => {
-                    eprintln!("falling back to clipboard_rs, wl-clipboard-rs failed with error {e}")
-                }
-            }
-        }
-        Err(e) => eprintln!("falling back to clipboard_rs, wl-clipboard-rs failed with error {e}"),
-    }
-
     let mut ctx_guard = get_clipboard_ctx()?;
     let ctx = ctx_guard
         .as_mut()
